f0bc06f523030e040649f03fb3b0ef011bde8229
Ryan Daley <rdaley321@yahoo.com>
2018-10-29 23:30:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,5 +9,5 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
-  array.each_with_index!{|x,i| "Hello, #{x}! You'll be assigned to room #{i}!"}
+  array.each_with_index{|x,i| "Hello, #{x}! You'll be assigned to room #{i}!"}
 end
