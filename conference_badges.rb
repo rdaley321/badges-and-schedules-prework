69e7c4cd336c219edf5435dc4841f3a228850ecb
Ryan Daley <rdaley321@yahoo.com>
2018-10-29 23:15:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,9 @@
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name) 
-  
+  "Hello, my name is #{name}"
+end
+
+def batch_badge_maker(array)
+  array.map
 end
