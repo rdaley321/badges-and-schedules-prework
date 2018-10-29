cae7378e937dfc420c4c53b6fe3f8c7e600a04e7
Ryan Daley <rdaley321@yahoo.com>
2018-10-29 23:48:49 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ def assign_rooms(array)
   new_array
 end
 
-def printer
-  batch_badge_creator.each{|x| puts x}
-  assign_rooms.each{|x| puts x}
+def printer(array)
+  batch_badge_creator(array).each{|x| puts x}
+  assign_rooms(array).each{|x| puts x}
 end
